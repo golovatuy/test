9da0ecbcb0d3afa4c25638e96e8595ace8117355
Oleksandr Golovatyi <golovatuy@gmail.com>
2021-03-31 08:56:16 +0300

    added test 4

--- server.js
+++ server.js
@@ -15,5 +15,9 @@ app.get('/test3', function(req, res) {
     res.sendFile(path.join(__dirname + '/index3.html'));
 });
 
+app.get('/test4', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index4.html'));
+});
+
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
